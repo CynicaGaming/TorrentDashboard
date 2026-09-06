afc09ae3d2e15f0f2bf2be308afdf86425f0efaa
Cynica <47292146+CynicaGaming@users.noreply.github.com>
2026-09-06 08:50:57 -0400

    Stabilize default notification sound mapping

--- static/default-notification-sound.js
+++ static/default-notification-sound.js
@@ -4,30 +4,31 @@
   if (window.__tdDefaultNotificationSoundPatched) return;
   window.__tdDefaultNotificationSoundPatched = true;
 
-  const replacement = '/static/notification-default.mp3';
-  const replaceDefaultSound = source => String(source || '').replace('/static/default-completion.wav', replacement);
+  const legacyPath = '/static/default-completion.wav';
+  const defaultPath = '/static/notification-default.mp3';
+
+  function replaceDefaultSound(source) {
+    if (typeof source !== 'string' || !source) return source;
+    try {
+      const url = new URL(source, window.location.href);
+      if (url.origin !== window.location.origin || url.pathname !== legacyPath) return source;
+      url.pathname = defaultPath;
+      return /^https?:\/\//i.test(source)
+        ? url.href
+        : `${url.pathname}${url.search}${url.hash}`;
+    } catch {
+      return source;
+    }
+  }
 
   const NativeAudio = window.Audio;
-  if (typeof NativeAudio === 'function') {
-    function TorrentDashboardAudio(source) {
-      return new NativeAudio(replaceDefaultSound(source));
-    }
-    TorrentDashboardAudio.prototype = NativeAudio.prototype;
-    Object.setPrototypeOf(TorrentDashboardAudio, NativeAudio);
-    window.Audio = TorrentDashboardAudio;
+  if (typeof NativeAudio !== 'function') return;
+
+  function TorrentDashboardAudio(source) {
+    return new NativeAudio(replaceDefaultSound(source));
   }
 
-  if (typeof window.playSoundUrl === 'function') {
-    const originalPlaySoundUrl = window.playSoundUrl;
-    window.playSoundUrl = function(source) {
-      return originalPlaySoundUrl.call(this, replaceDefaultSound(source));
-    };
-  }
-
-  if (typeof window.configuredCompletionSoundUrl === 'function') {
-    const originalConfiguredSoundUrl = window.configuredCompletionSoundUrl;
-    window.configuredCompletionSoundUrl = function() {
-      return replaceDefaultSound(originalConfiguredSoundUrl.call(this));
-    };
-  }
+  TorrentDashboardAudio.prototype = NativeAudio.prototype;
+  Object.setPrototypeOf(TorrentDashboardAudio, NativeAudio);
+  window.Audio = TorrentDashboardAudio;
 })();
